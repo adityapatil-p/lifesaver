47faf41f48d20915919199299370b5ee7a8a3644
Aditya <adityapatil242004@gmail.com>
2026-06-29 15:21:12 +0530

    Connect frontend to Cloud Run backend

--- Last-Minute-Lifesaver/frontend/src/services/api.js
+++ Last-Minute-Lifesaver/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://lifesaver-293675081153.asia-south1.run.app",
     headers: {
         "Content-Type": "application/json",
     },
